3d4fa8e5a7ad355e51fd1f37f451d2e1627f5769
dineshbias <dinesh.joshi@globant.com>
2019-01-08 16:35:57 +0530

    Plugins updated in pom.xml

---
+++ BasicProjectSetup/src/test/java/demo/setup/TestTeacher.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package demo.setup;
+
+import org.junit.Test;
+
+/**
+ * @author dinesh.joshi
+ *
+ */
+public class TestTeacher {
+
+  @Test
+  public void test() {
+    // assertTrue(false);
+  }
+
+}
